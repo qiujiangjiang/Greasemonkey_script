fb0a9c9fcf9a180270f5fe155dd5927dec79385a
copyman <48672845+qiujiangjiang@users.noreply.github.com>
2025-09-09 10:57:19 +0800

    Create test.user.js

---
+++ test.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         My First Script
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A simple script to try out!
+// @author       You
+// @match        https://www.google.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    alert('Hello, Google!');
+})();
