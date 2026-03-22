ad27caf7817e455bc31aa6726b2d5bccd74616ed
copyman <48672845+qiujiangjiang@users.noreply.github.com>
2026-03-22 11:35:18 +0800

    Update 小红书广告数据查询与展示.user.js

--- 小红书广告数据查询与展示.user.js
+++ 小红书广告数据查询与展示.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         qjjtools
 // @namespace    npm/vite-plugin-monkey
-// @version      18.5.5
+// @version      18.5.6
 // @author       qjj
 // @description  qjjtools
 // @license      MIT
